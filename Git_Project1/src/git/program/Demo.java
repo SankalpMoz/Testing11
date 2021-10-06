91b63e04f876fb9f533fcc83baaf996525786c51
MY PC <MY PC@LAPTOP-ROB8A425>
2021-10-06 11:08:31 +0530

    by S

--- Git_Project1/src/git/program/Demo.java
+++ Git_Project1/src/git/program/Demo.java
@@ -2,6 +2,12 @@ package git.program;
 
 public class Demo {
 
+	public void m1()
+	{
+		System.out.println("Hello this is M1");
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
