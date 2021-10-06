9fb5a9c4cacb3b19a5710417940452b99a184316
SankalpMoz <91942202+SankalpMoz@users.noreply.github.com>
2021-10-06 15:22:03 +0530

    sankalp

--- Selenium_GIT_Project1/src/git/programs/Demo1.java
+++ Selenium_GIT_Project1/src/git/programs/Demo1.java
@@ -12,12 +12,18 @@ public class Demo1 {
 		System.out.println("Hello this is M2");
 	}
 	
+	public void m3()
+	{
+		System.out.println("Hello this is M3");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
 		Demo1 d=new Demo1();
 		d.m1();
 		d.m2();
+		d.m3();
 	}
 
 }
