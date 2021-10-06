18d7cea09fccb75b56093524fa5d047413b9e366
MY PC <MY PC@LAPTOP-ROB8A425>
2021-10-06 11:33:43 +0530

    By M

--- Selenium_GIT_Project1/src/git/programs/Demo1.java
+++ Selenium_GIT_Project1/src/git/programs/Demo1.java
@@ -2,6 +2,11 @@ package git.programs;
 
 public class Demo1 {
 	
+	public void m1()
+	{
+		System.out.println("Hello this is M1");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Helloe Git");
